fe4221f9f3f1c97cfd9197d8ea19b20fb3b93f09
NoahKeijzerAvans <109356254+NoahKeijzerAvans@users.noreply.github.com>
2023-02-16 00:44:43 +0100

    Add Changes and Commit working

--- Program.cs
+++ Program.cs
@@ -3,23 +3,15 @@
 using Domain.Models;
 using DomainServices.Utils;
 
-var changes = new List<Change>
-{
-    new Change()
-};
-var newChanges = new List<Change>
-{
-    new Change()
-};
-
 var changeTracker = new ChangesTracker();
-changeTracker.AddChanges(changes);
-changeTracker.AddChanges(newChanges);
-changeTracker.CommitChanges(changeTracker.Changes);
-changeTracker.AddChanges(newChanges);
-changeTracker.AddChanges(newChanges);
-changeTracker.CommitChanges(changeTracker.Changes);
-// changeTracker.Commits.ForEach(c => Console.WriteLine(c.ToString()));
+
+changeTracker.AddChange(new Change());
+changeTracker.AddChange(new Change());
+changeTracker.CommitChanges();
+changeTracker.AddChange(new Change());
+changeTracker.AddChange(new Change());
+changeTracker.CommitChanges();
+
 
 
 
