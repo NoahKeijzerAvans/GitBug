ce149bd69b059bc46741b9e5b0339ab8d44b0e8d
NoahKeijzerAvans <109356254+NoahKeijzerAvans@users.noreply.github.com>
2023-02-15 18:40:18 +0100

    Opzetje States

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Domain.Models;
+using DomainServices.Utils;
+
+var changes = new List<Change>
+{
+    new Change()
+};
+var newChanges = new List<Change>
+{
+    new Change()
+};
+
+var changeTracker = new ChangesTracker();
+changeTracker.AddChanges(changes);
+changeTracker.AddChanges(newChanges);
+changeTracker.CommitChanges(changeTracker.Changes);
+changeTracker.AddChanges(newChanges);
+changeTracker.AddChanges(newChanges);
+changeTracker.CommitChanges(changeTracker.Changes);
+// changeTracker.Commits.ForEach(c => Console.WriteLine(c.ToString()));
+
+
+
