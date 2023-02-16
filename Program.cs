68366716e54d686e4a49b251b70b96d60bb6a42b
NoahKeijzerAvans <109356254+NoahKeijzerAvans@users.noreply.github.com>
2023-02-16 02:02:23 +0100

    Branching is working

--- Program.cs
+++ Program.cs
@@ -4,13 +4,18 @@ using Domain.Models;
 using DomainServices.Utils;
 
 var changeTracker = new ChangesTracker();
+changeTracker.GetCurrentBranch();
+changeTracker.CreateBranch();
 
 changeTracker.AddChange(new Change());
 changeTracker.AddChange(new Change());
 changeTracker.CommitChanges();
+
 changeTracker.AddChange(new Change());
 changeTracker.AddChange(new Change());
 changeTracker.CommitChanges();
+
+var idk = "hello";
 
 
 
