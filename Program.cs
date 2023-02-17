3fbf4c6c28fa4a082db9b7103cadf1362f1c05ef
NoahKeijzerAvans <109356254+NoahKeijzerAvans@users.noreply.github.com>
2023-02-17 17:17:27 +0100

    Command pattern

--- Program.cs
+++ Program.cs
@@ -1,22 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Domain.Models;
-using DomainServices.Utils;
+﻿using DomainServices.Utils;
 
 var changeTracker = new ChangesTracker();
-changeTracker.GetCurrentBranch();
-changeTracker.CreateBranch();
+var commandListener = new CommandControl(changeTracker);
 
-changeTracker.AddChange(new Change());
-changeTracker.AddChange(new Change());
-changeTracker.CommitChanges();
-
-changeTracker.AddChange(new Change());
-changeTracker.AddChange(new Change());
-changeTracker.CommitChanges();
-
-var idk = "hello";
-
+commandListener.Listen();
 
 
 
