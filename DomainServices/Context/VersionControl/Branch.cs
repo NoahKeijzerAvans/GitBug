dce08818bb790f95e6396915bbb6d54205578c05
NoahKeijzerAvans <109356254+NoahKeijzerAvans@users.noreply.github.com>
2023-02-24 22:47:59 +0100

    Changed Name to VersionControl

---
+++ DomainServices/Context/VersionControl/Branch.cs
@@ -0,0 +1,21 @@
+﻿using DomainServices.Interfaces.Change;
+
+namespace DomainServices.Context.VersionControl
+{
+    public class Branch
+    {
+        public string Name { get; }
+        public Dictionary<Commit, IChangesState> Commits { get; }
+
+        public Branch(string name, Dictionary<Commit, IChangesState> commits)
+        {
+            Name = name;
+            Commits = commits;
+        }
+        public Branch(string name)
+        {
+            Name = name;
+            Commits = new Dictionary<Commit, IChangesState>();
+        }
+    }
+}
---
+++ DomainServices/Context/VersionControl/Change.cs
@@ -0,0 +1,16 @@
+﻿namespace DomainServices.Context.VersionControl;
+public class Change
+{
+    public dynamic Content { get; }
+
+    public Change(dynamic content)
+    {
+        Content = content;
+    }
+
+    public Change()
+    {
+        Content = new object();
+    }
+}
+
