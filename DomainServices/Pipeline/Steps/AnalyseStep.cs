f536708f352318fba82a8649d20f8ba72d5f9cfe
NoahKeijzerAvans <109356254+NoahKeijzerAvans@users.noreply.github.com>
2023-03-27 23:34:56 +0200

    Pipeline steps added and functioning with loader

---
+++ DomainServices/Pipeline/Steps/AnalyseStep.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using DomainServices.Interfaces.Pipeline;
+
+namespace DomainServices.Pipeline.Steps;
+
+public class AnalyseStep : IPipelineStep
+{
+    public void Excecute()
+    {
+        Console.WriteLine("Excecuting Analyse step, please wait.");
+        ConsoleSpinner.Draw();
+        Console.WriteLine("\nAnalyse step succeeded!");
+    }
+    public override string ToString()
+    {
+        return "Analyse";
+    }
+}
