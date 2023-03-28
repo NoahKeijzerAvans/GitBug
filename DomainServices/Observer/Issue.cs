7e59652a63be4cf150fc591a284634366f31ca0b
TimdeLaater <tdelaater@gmail.com>
2023-03-28 21:50:18 +0200

    Started on Thread and updates on issues

---
+++ DomainServices/Observer/Issue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainServices.Observer
+{
+    public class Issue
+    {
+        private Thread thread;
+        public Thread addThread()
+        {
+            return new Thread();
+        }
+        public void addComment(string comment, string choiceOfNotification)
+        {
+            IObserver observer = new SlakNotification();
+            thread.subscribe(observer);
+            thread.addComment(comment);
+            thread.notify(comment);
+
+        }
+    }
+}
